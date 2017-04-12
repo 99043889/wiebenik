8ee1be79ad791a2717f1ced388a51c0e7c350c57
louiscocks <99043889@mydavinci.nl>
2017-04-12 10:55:45 +0200

    updatePortfolio

---
+++ wiejava.js
@@ -0,0 +1,19 @@
+    function goHome() {
+    window.location.assign("oefening-1.html")
+}
+
+function goOpleiding() {
+    window.location.assign("opleiding.html")
+}
+
+function goOverMij() {
+    window.location.assign("opleiding.html")
+}
+
+function goBeroep() {
+    window.location.assign("opleiding.html")
+}
+
+function goNieuws() {
+    window.location.assign("opleiding.html")
+}
